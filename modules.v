1c0f6e79654cab9263f2effc33fc6a89daad1bcd
Paszkal <37273976+Paszkal@users.noreply.github.com>
2019-04-08 16:23:00 +0200

    Update modules.v

--- modules.v
+++ modules.v
@@ -139,6 +139,26 @@ module data_reg(
 	
 endmodule
 
+//right shift reg module
+module shift_reg (
+  input clk,
+  input rst, 
+  input [7:0] data,
+  output data_out
+)
+  reg [7:0] temp;
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+        temp<=data[0];
+      else
+        temp<=temp[0,6:1];
+    end
+  
+  assign data_out=temp[0];
+endmodule
+
 
 
 
